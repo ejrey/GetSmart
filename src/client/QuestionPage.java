611f68821f2cbf2d79dc82b278d0894932a71849
MaxiKolbe <randyletran@gmail.com>
2022-07-19 22:21:46 -0700

    Update QuestionPage

--- src/client/QuestionPage.java
+++ src/client/QuestionPage.java
@@ -5,36 +5,105 @@ import java.awt.*;
 
 public class QuestionPage {
 
-    public QuestionPage() {
+    public QuestionPage(String question, String questionNumber, String[] answers) {
 
-        String frameName = "Test Page";
-        String buttonText = "Click me";
-        String labelName = "label text here";
+        String frameName = questionNumber;
+        String labelName = question;
+        String[] buttonAnswers = answers;
+        
+        buttonAnswers = formatAnswers(buttonAnswers);
 
-        // Create frame and define the size, holds components
         JFrame questionFrame = new JFrame(frameName);
         questionFrame.setSize(1280, 720);
-        // Defines behaviour of frame upon being closed
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        questionFrame.add(mainPanel);
+
         questionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // JTextField allows editing of a single line of text
-        JTextField questionText = new JTextField(50);
-        questionFrame.add(questionText, BorderLayout.NORTH);
+        JPanel questionTitlePanel = new JPanel();
+        mainPanel.add(questionTitlePanel);
 
-        // JLabel displays information (can be text and/or image) and does not accept user input
-        JLabel answerText = new JLabel(labelName, SwingConstants.CENTER);
-        questionFrame.add(answerText, BorderLayout.CENTER);
+        questionTitlePanel.setBorder(BorderFactory.createEmptyBorder(50,10,10,10));
+        questionTitlePanel.setSize(200,200);
 
-        // JButton allows users to “ push”/“click” to signify an action
-        JButton button = new JButton(buttonText);
-        questionFrame.add(button, BorderLayout.SOUTH);
+        JLabel questionLabel = new JLabel(labelName);
+        questionLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        questionTitlePanel.add(questionLabel);
 
-        // By default, it is set to false, need to set it to true in order to see
+        JPanel questionAnswerPanel = new JPanel();
+        questionAnswerPanel.setLayout(new BoxLayout(questionAnswerPanel, BoxLayout.Y_AXIS));
+        questionAnswerPanel.setBorder(BorderFactory.createEmptyBorder(50,50,100,50));
+
+        JPanel questionAnswerOnePanel = new JPanel();
+        questionAnswerOnePanel.setLayout((new BoxLayout(questionAnswerOnePanel, BoxLayout.X_AXIS)));
+        questionAnswerOnePanel.setBorder(BorderFactory.createEmptyBorder(25,10,25,10));
+
+        JPanel questionAnswerTwoPanel = new JPanel();
+        questionAnswerTwoPanel.setLayout((new BoxLayout(questionAnswerTwoPanel, BoxLayout.X_AXIS)));
+        questionAnswerTwoPanel.setBorder(BorderFactory.createEmptyBorder(25,10,25,10));
+
+        JPanel questionAnswerThreePanel = new JPanel();
+        questionAnswerThreePanel.setLayout((new BoxLayout(questionAnswerThreePanel, BoxLayout.X_AXIS)));
+        questionAnswerThreePanel.setBorder(BorderFactory.createEmptyBorder(25,10,25,10));
+
+        JPanel questionAnswerFourPanel = new JPanel();
+        questionAnswerFourPanel.setLayout((new BoxLayout(questionAnswerFourPanel, BoxLayout.X_AXIS)));
+        questionAnswerFourPanel.setBorder(BorderFactory.createEmptyBorder(25,10,25,10));
+
+        questionAnswerPanel.setSize(200, 100);
+
+        JButton buttonOne = new JButton("1");
+        buttonOne.setLayout(new FlowLayout(FlowLayout.LEFT, 25, 5));
+        JLabel answerOne = new JLabel(buttonAnswers[0]);
+        answerOne.setMaximumSize(new Dimension(800, 25));
+
+        JButton buttonTwo = new JButton("2");
+        JLabel answerTwo = new JLabel(buttonAnswers[1]);
+        answerTwo.setMaximumSize(new Dimension(800, 25));
+
+        JButton buttonThree = new JButton("3");
+        JLabel answerThree = new JLabel(buttonAnswers[2]);
+        answerThree.setMaximumSize(new Dimension(800, 25));
+
+        JButton buttonFour = new JButton("4");
+        JLabel answerFour = new JLabel(buttonAnswers[3]);
+        answerFour.setMaximumSize(new Dimension(800, 25));
+
+        questionAnswerOnePanel.add(buttonOne);
+        questionAnswerOnePanel.add(answerOne);
+
+        questionAnswerTwoPanel.add(buttonTwo);
+        questionAnswerTwoPanel.add(answerTwo);
+
+        questionAnswerThreePanel.add(buttonThree);
+        questionAnswerThreePanel.add(answerThree);
+
+        questionAnswerFourPanel.add(buttonFour);
+        questionAnswerFourPanel.add(answerFour);
+
+        questionAnswerPanel.add(questionAnswerOnePanel);
+        questionAnswerPanel.add(questionAnswerTwoPanel);
+        questionAnswerPanel.add(questionAnswerThreePanel);
+        questionAnswerPanel.add(questionAnswerFourPanel);
+
+        mainPanel.add(questionAnswerPanel);
+
         questionFrame.setVisible(true);
     }
 
+    private String[] formatAnswers(String[] buttonAnswers) {
+        String[] tempArray = new String[buttonAnswers.length];
+        for(int i = 0; i < buttonAnswers.length; i++) {
+            tempArray[i] = "   " + buttonAnswers[i];
+        }
+        return tempArray;
+    }
+
     public static void main(String[] args){
-        new QuestionPage();
+        String[] arr = new String[]{"Yo", "Hi", "Sup", "Hey"};
+        new QuestionPage("Question here", "1", arr);
     }
 
 }
