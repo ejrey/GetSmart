0d4f086072b18d96a4afe18e5818a43c1c88e858
ejrey <dayrey24@gmail.com>
2022-07-25 23:53:46 -0700

    Able to interact on board (a bit)

--- src/client/Board.java
+++ src/client/Board.java
@@ -2,8 +2,10 @@ package client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Board {
+public class Board implements ActionListener {
 
     public Board() {
 
@@ -36,6 +38,9 @@ public class Board {
         addColumnToMainPanel(gameBoard, mainPanel, colFour, 4);
         addColumnToMainPanel(gameBoard, mainPanel, colFive, 5);
         addColumnToMainPanel(gameBoard, mainPanel, colSix, 6);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(50,175,0,100));
+
+
 
     }
 
@@ -45,17 +50,36 @@ public class Board {
         JLabel columnTitle = new JLabel("Col " + colNumber);
         columnTitle.setFont(new Font("Verdana", Font.PLAIN, 20));
         column.add(columnTitle);
+        int pointValue = 100;
         for (int i = 0; i < 5; i++) {
-            JButton buttonQuestion = new JButton("C" + colNumber + ": " + i);
+//            JButton buttonQuestion = new JButton("C" + colNumber + ": " + i);
+            JLabel buttonLabel = new JLabel("" + pointValue);
+            JButton buttonQuestion = new JButton(colNumber + "-" + pointValue);
+            pointValue = pointValue + 100;
             buttonQuestion.setMaximumSize(new Dimension (250, 100));
             buttonQuestion.setBorder(BorderFactory.createCompoundBorder(
-                    BorderFactory.createLineBorder(Color.CYAN, 20),
-                    BorderFactory.createEmptyBorder(20, 0, 20, 10)));
+                    BorderFactory.createLineBorder(Color.DARK_GRAY, 20),
+                    BorderFactory.createEmptyBorder(20, 0, 20, 0)));
             column.add(buttonQuestion);
+            buttonQuestion.addActionListener((ActionListener) this);
         }
         column.setLayout(new BoxLayout(column, BoxLayout.Y_AXIS));
         gameBoard.add(mainPanel);
 //        column.setBackground(Color.BLACK);
-        column.setBorder(BorderFactory.createEmptyBorder(50,45,50,45));
+//        column.setBorder(BorderFactory.createEmptyBorder(50,45,50,45));
+
     }
+
+    // To identify which button is pressed
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("1-100")) {
+            String[] test = {"yo","yo","yo","yo"};
+            new QuestionPage("Hi", "1", test);
+        } else if (e.getActionCommand().equals("Exit")) {
+//            System.exit(0);
+        }
+        System.out.println(e.getActionCommand());
+    }
+
 }
