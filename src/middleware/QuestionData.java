4a9a587c5fe6d3ae1034b125482b46dd48d98930
ejrey <dayrey24@gmail.com>
2022-08-01 22:26:39 -0700

    Updated small code

--- src/middleware/QuestionData.java
+++ src/middleware/QuestionData.java
@@ -1,6 +1,12 @@
 package middleware;
 
 public class QuestionData {
+    public enum ButtonState {
+        LOCKED,
+        UNLOCKED,
+        ANSWERED,
+    }
+
     public String question;
     public int row;
     public int col;
